f0ca0496a2f2ece47555f66d5df4bbed03e83172
abelfd <abel.flores@institutvidreres.cat>
2021-11-23 08:38:15 +0100

    Exvariables1

---
+++ JSex1.js
@@ -0,0 +1,85 @@
+/*Ex1 
+console.log("Abel")
+console.warn("Flores Diaz")
+console.error("27/12/2002")
+*/
+
+/*Ex2
+var variableuno = "Hola"
+let variabledos = "Hola como estas"
+const variabletres = "Deuu"
+console.log(variableuno)
+console.log(variabledos)
+console.log(variabletres)
+*/
+
+/*Ex3
+var variable = "Hola"
+console.log(variable)
+*/
+
+/*Ex4
+var variable = 34
+console.log(variable)
+*/
+
+/*Ex5
+const CONSTANTE = 'abelfd'
+console.log(CONSTANTE)
+CONSTANTE = 'yo'
+*/
+
+/*Ex6
+let numero1 = 1234
+let numero2 = 5678
+console.log((numero1 + numero2).toFixed(1))
+console.log((numero1 - numero2).toFixed(1))
+console.log((numero1 * numero2).toFixed(1))
+console.log((numero1 / numero2).toFixed(1))
+*/
+
+/*Ex7
+let catetos = 4;
+console.log((catetos * catetos) + (catetos * catetos))
+*/
+
+/*Ex8
+var numero1 = 45;
+var string = 'abel'
+console.log(numero1/string)
+*/
+
+/*Ex9
+var nom = 'Abel Flores Diaz'
+var cog = nom.slice(5,10)
+console.log(cog)
+*/
+
+/*Ex10
+var nombre = 'Abel Flores Diaz'
+var elimina = nombre.replace('A','')
+console.log(elimina)
+*/
+
+/*Ex11
+var variable1 = undefined
+var variable2 = null
+console.log(variable1 + variable2)
+*/
+
+
+/*Ex12
+console.log(String(true))
+console.log(String + true)
+*/
+
+/*Ex13
+console.log(Boolean('Como estas?'))
+console.log(true ||'bon dia')
+*/
+
+
+/*Ex14
+console.log(Number(true))
+console.log(45 != true)
+*/
